bc5b62f8d6f651be299846af8a2fe886adf9c698
Salmane KHERCHOUCH <salmane.kherchouch@hotmail.com>
2022-03-14 16:58:52 +0100

    Ajout du component HelloWord

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import './component/HelloWorld';
+import HelloWorld from './component/HelloWorld';
+
+
 
 function App() {
+  const [hello, setHello] = useState(false);
+
+
+
   return (
     <div className="App">
       <header>
         <p>
           TodoList
         </p>
-        <button>Ajouter</button>
+        <button onClick={() => setHello(true)}>Ajouter</button>        
         <button>Supprimer</button>
+        {hello&&<HelloWorld />}
       </header>
     </div>
   );
